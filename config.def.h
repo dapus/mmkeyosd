5f4156376c3e717ea575caa74f5cdaa841bad935
David Dufberg <david@dufberg.se>
2012-04-09 22:21:20 +0200

    Added some explanation to config.def.h

--- config.def.h
+++ config.def.h
@@ -24,8 +24,15 @@ char mpd_cmd_toggle[] = "mpc toggle | sed -n -e 's/^\\[\\(.*\\)\\].*/\\1/p' | tr
 char mpd_cmd_next[] = "mpc next>/dev/null; s=`mpc current`; [ \"$s\" ] && echo $s || echo Nothing to Play";
 char mpd_cmd_prev[] = "mpc prev>/dev/null; s=`mpc current`; [ \"$s\" ] && echo $s || echo Nothing to Play";
 
+/*
+ * Note: the shell command stdout is passed to
+ * the selected display function.
+ * text_with_bar needs a number between 0 and 100.
+ * text_with_text needs a string.
+ */
 struct config
 conf[] = {
+	/* Key                           Text                 Display func          Shell command */
 	{XF86XK_MonBrightnessUp,         "Brightness",        text_with_bar,        br_cmd},
 	{XF86XK_MonBrightnessDown,       "Brightness",        text_with_bar,        br_cmd},
 	{XF86XK_AudioLowerVolume,        "Volume",            text_with_bar,        vol_cmd_dec},
