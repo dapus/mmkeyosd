e031862c86d135ea58cb02119a34cd040309bdcf
David Dufberg <david@dufberg.se>
2012-04-12 04:29:37 +0200

    Display error msg in window if commands fails

--- config.def.h
+++ config.def.h
@@ -3,12 +3,13 @@ char *fontstrbig = "-*-dejavu sans mono-medium-r-*-*-20-*-*-*-*-*-*-*";
 char *fontstrsmall = "-*-dejavu sans mono-medium-r-*-*-13-*-*-*-*-*-*-*";
 char *fgcolor = "white";
 char *bgcolor = "black";
+char *errcolor = "red";
 int bw = 0;             /* border width    */
 int ww = 300;           /* window width    */
 int wh = 150;           /* window height   */
 int barw = 150;         /* bar width       */
 int barh = 15;          /* bar height      */
-float opacity = 0.7;    /* window opacity  */
+float opacity = 0.8;    /* window opacity  */
 
 /* this script doesn't actually change
  * brightness (because my hardware does
@@ -20,12 +21,16 @@ BRMAX=`cat $BRMAXFILE`\n\
 BR=`cat $BRFILE`\n\
 echo | awk \"END{print $BR/$BRMAX*100}\"";
 
-char vol_cmd_dec[] = "ossvol.sh -2 | awk \"END{print \\$0/25*100}\"";
-char vol_cmd_inc[] = "ossvol.sh +2 | awk \"END{print \\$0/25*100}\"";
-char vol_cmd_mute[] = "ossvol.sh -t";
-char mpd_cmd_toggle[] = "mpc toggle | sed -n -e 's/^\\[\\(.*\\)\\].*/\\1/p' | tr '[=p=]' P";
+char vol_cmd_dec[] = "amixer set Master 5%- | sed -n -e 's/.*Playback.*\\[\\([0-9]*\\)%\\].*/\\1/p' |  head -n 1";
+char vol_cmd_inc[] = "amixer set Master 5%+ | sed -n -e 's/.*Playback.*\\[\\([0-9]*\\)%\\].*/\\1/p' |  head -n 1";
+char vol_cmd_mute[] = "amixer set Master toggle | sed -n -e 's/.*Playback.*\\[\\(on\\|off\\)\\].*/\\1/p' | head -n 1 | awk '{ if($0 == \"off\") print \"Muted\"; else print \"Unmuted\"}'";
+//char vol_cmd_dec[] = "ossvol.sh -2 | awk \"END{print \\$0/25*100}\"";
+//char vol_cmd_inc[] = "ossvol.sh +2 | awk \"END{print \\$0/25*100}\"";
+//char vol_cmd_mute[] = "ossvol.sh -t";
+char mpd_cmd_toggle[] = "mpc toggle | sed -n -e 's/^\\[\\(.*\\)\\].*/\\1/p' | tr '[=p=]' P | awk '{if($0 == \"Playing\"){printf(\"%s: \", $0); system(\"mpc current\")} else print $0}'";
 char mpd_cmd_next[] = "mpc next>/dev/null; s=`mpc current`; [ \"$s\" ] && echo $s || echo Nothing to Play";
 char mpd_cmd_prev[] = "mpc prev>/dev/null; s=`mpc current`; [ \"$s\" ] && echo $s || echo Nothing to Play";
+char wifi_toggle[] = "[ \"`cat /sys/class/rfkill/rfkill2/state`\" -eq 1 ] && echo On || echo Off";
 
 /*
  * Note: the shell command stdout is passed to
@@ -40,10 +45,13 @@ conf[] = {
     {0,        XF86XK_MonBrightnessDown,       "Brightness",        text_with_bar,        br_cmd},
     {0,        XF86XK_AudioLowerVolume,        "Volume",            text_with_bar,        vol_cmd_dec},
     {0,        XF86XK_AudioRaiseVolume,        "Volume",            text_with_bar,        vol_cmd_inc},
-    {0,        XF86XK_AudioMute,               "Volume",            text_with_text,       "ossvol.sh -t"},
+    {0,        XF86XK_AudioMute,               "Volume",            text_with_text,       vol_cmd_mute},
     {0,        XF86XK_AudioPlay,               "MPD",               text_with_text,       mpd_cmd_toggle},
     {0,        XF86XK_AudioNext,               "MPD",               text_with_text,       mpd_cmd_next},
     {0,        XF86XK_AudioPrev,               "MPD",               text_with_text,       mpd_cmd_prev},
-    {Mod1Mask|ControlMask, XK_m,               "Message",           text_with_text,       "echo \"This is a test :3\""},
+    {0,        XF86XK_WLAN,                    "Wifi",              text_with_text,       wifi_toggle},
+	/* These are just for testing :) */
+    {Mod1Mask|ControlMask, XK_m,               "Message",           text_with_text,       "/in/echo \"This is a test :3\""},
+    {Mod1Mask|ControlMask, XK_t,               "Message",           text_with_text,       "echo askdj 1>&2"},
 };
 
--- mmkeyosd.c
+++ mmkeyosd.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <poll.h>
+#include <sys/wait.h>
 
 #include <X11/X.h>
 #include <X11/Xlib.h>
@@ -18,13 +20,14 @@
 #define die(fmt, ...) do { fprintf(stderr, fmt, ##__VA_ARGS__); exit(1); } while(0)
 #define CENTER(A, B) (( (A)/2 ) - ( (B)/2 ))
 #define CLEANMASK(mask) (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
 
 struct
 config {
 	unsigned int mod;
 	KeySym key;
 	char *text;
-	void (*disp)(struct config *, char *);
+	void (*disp)(struct config *, char *, int error);
 	char *cmd;
 };
 
@@ -34,8 +37,8 @@ font {
 	int h;
 };
 
-void text_with_bar(struct config *c, char *in);
-void text_with_text(struct config *c, char *in);
+void text_with_bar(struct config *c, char *in, int error);
+void text_with_text(struct config *c, char *in, int error);
 
 #include "config.h"
 
@@ -54,11 +57,13 @@ Atom NetWMWindowOpacity;
 unsigned int numlockmask = 0;
 int wx, wy, sw, sh;
 
-unsigned long fgcol;
-unsigned long bgcol;
+XftColor fgcol;
+XftColor bgcol;
+XftColor errcol;
 
 Bool is_mapped = False;
-
+int pid = -1;
+int cmdst = 0;
 
 int
 handle_xerror(Display * display, XErrorEvent * e) {
@@ -78,20 +83,12 @@ setup_font(struct font *font, char *fontstr) {
 }
 
 void
-draw_text(struct font *font, char *str, int x, int y) {
-	XftColor color;
+draw_text(struct font *font, XftColor *col, char *str, int x, int y) {
 	XftDraw *draw;
 
-	color.color.red = (fgcol >> 16) * 0x101;
-	color.color.green = (fgcol >> 8) * 0x101;
-	color.color.blue = fgcol * 0x101;
-	color.color.alpha = 0xFFFF;
-	color.pixel = 0xFFFFFF00;
-
-	XftColorAllocValue(dpy, visual, cmap, &color.color, &color);
 	draw = XftDrawCreate(dpy, win, visual, cmap);
 
-	XftDrawStringUtf8(draw, &color, font->xfont, x, y, (XftChar8 *)str, strlen(str));
+	XftDrawStringUtf8(draw, col, font->xfont, x, y, (XftChar8 *)str, strlen(str));
 }
 
 int
@@ -102,10 +99,8 @@ text_width(struct font *font, char *str) {
 	return ext.xOff;
 }
 
-#define MAX(A, B) ((A) > (B) ? (A) : (B))
-
 void 
-text_with_text(struct config *c, char *in) {
+text_with_text(struct config *c, char *in, int error) {
 	int nww, tws, twb;
 
 	tws = text_width(&fontsmall, in);
@@ -115,37 +110,41 @@ text_with_text(struct config *c, char *in) {
 	XMoveResizeWindow(dpy, win, CENTER(sw, nww), wy, nww, wh);
 
 	/* Clear window */
-	XSetForeground(dpy, gc, bgcol);
+	XSetForeground(dpy, gc, bgcol.pixel);
 	XFillRectangle(dpy, win, gc, 0, 0, nww, wh);
 
-	XSetForeground(dpy, gc, fgcol);
-	draw_text(&fontbig, c->text, CENTER(nww, text_width(&fontbig, c->text)),
+	draw_text(&fontbig, &fgcol, c->text, CENTER(nww, text_width(&fontbig, c->text)),
 			CENTER(wh/2, fontbig.h)+fontbig.h);
-	draw_text(&fontsmall, in, CENTER(nww, text_width(&fontsmall, in)), (wh/2)+fontsmall.h);
+	draw_text(&fontsmall, error ? &errcol : &fgcol, in, CENTER(nww, text_width(&fontsmall, in)), (wh/2)+fontsmall.h);
 
 	XSync(dpy, False);
 }
 
 void 
-text_with_bar(struct config *c, char *in) {
+text_with_bar(struct config *c, char *in, int error) {
 	int nww, twb;
+
+	/* If there was an error, display it instead of drawing a bar */
+	if(error) {
+		text_with_text(c, in, error);
+		return;
+	}
 
 	twb = text_width(&fontbig, c->text);
 	nww = MAX(ww, twb + 30);
 
 	XMoveResizeWindow(dpy, win, CENTER(sw, nww), wy, nww, wh);
 
-	XRectangle r = { CENTER(nww, barw), /*CENTER(wh/2, barh)+*/(wh/2), barw, barh };
+	XRectangle r = { CENTER(nww, barw), (wh/2), barw, barh };
 
 	/* Clear window */
-	XSetForeground(dpy, gc, bgcol);
+	XSetForeground(dpy, gc, bgcol.pixel);
 	XFillRectangle(dpy, win, gc, 0, 0, nww, wh);
 
-	XSetForeground(dpy, gc, fgcol);
-	draw_text(&fontbig, c->text, CENTER(nww, text_width(&fontbig, c->text)),
+	draw_text(&fontbig, &fgcol, c->text, CENTER(nww, text_width(&fontbig, c->text)),
 			CENTER(wh/2, fontbig.h)+fontbig.h);
-//	printf("font height: %i\n", fonth);
 
+	XSetForeground(dpy, gc, fgcol.pixel);
 	/* border */
 	XDrawRectangles(dpy, win, gc, &r, 1);
 
@@ -154,7 +153,6 @@ text_with_bar(struct config *c, char *in) {
 	XFillRectangles(dpy, win, gc, &r, 1);
 
 	XSync(dpy, False);
-	//puts(in);
 }
 
 void
@@ -172,13 +170,10 @@ grabkey(unsigned int modmask, KeySym key) {
 	}
 };
 
-unsigned long
-getcolor(char *colstr) {
-	XColor color;
-
-	if(!XAllocNamedColor(dpy, cmap, colstr, &color, &color))
-		die("error, cannot allocate color '%s'\n", colstr);
-	return color.pixel;
+void
+getcolor(char *colstr, XftColor *color) {
+	if(!XftColorAllocName(dpy, visual, cmap, colstr, color))
+		die("ERROR: cannot allocate color '%s'\n", colstr);
 }
 
 void
@@ -188,6 +183,17 @@ sigalrm(int i) {
 	XFlush(dpy);
 	is_mapped = False;
 	//puts("Caught sigalrm");
+}
+
+void
+sigchld(int i) {
+	int p;
+
+	p = wait(NULL);
+	if(p == pid)
+		pid = -1;
+
+	signal(SIGCHLD, sigchld);
 }
 
 void
@@ -212,8 +218,8 @@ setup() {
 	wy = CENTER(sh, wh);
 
 	wattr.override_redirect = True;
-	wattr.background_pixel = bgcol; 
-	wattr.border_pixel = fgcol;
+	wattr.background_pixel = bgcol.pixel; 
+	wattr.border_pixel = fgcol.pixel;
 	win = XCreateWindow(dpy, root, wx, wy, ww, wh, bw,
 			CopyFromParent, InputOutput, CopyFromParent,
 			CWOverrideRedirect | CWBackPixel | CWBorderPixel, &wattr);
@@ -232,14 +238,14 @@ setup() {
 	for(i=0; i < LENGTH(conf); i++)
 		grabkey(conf[i].mod, conf[i].key);
 
-	/* behöver inte denna va? */
 	XSelectInput(dpy, root, KeyPressMask | SubstructureNotifyMask);
 
 	setup_font(&fontbig, fontstrbig);
 	setup_font(&fontsmall, fontstrsmall);
 
-	fgcol = getcolor(fgcolor);
-	bgcol = getcolor(bgcolor);
+	getcolor(fgcolor, &fgcol);
+	getcolor(bgcolor, &bgcol);
+	getcolor(errcolor, &errcol);
 
 	NetWMWindowOpacity = XInternAtom(dpy, "_NET_WM_WINDOW_OPACITY", False);
 	if(opacity > 1.0 || opacity < 0.0)
@@ -249,6 +255,7 @@ setup() {
 			PropModeReplace, (unsigned char *)real_opacity, 1);
 
 	signal(SIGALRM, sigalrm);
+	signal(SIGCHLD, sigchld);
 }
 
 void
@@ -261,37 +268,118 @@ start_timer() {
 		die("Could not set timer: %s\n", strerror(errno));
 }
 
+/* this function reads both stdout and stderr
+ * from cmd. this way we can capture error messages.
+ * it also has a time out to prevent lock ups */
+void
+readcmd(char *cmd, char *stdoutbuf, int stdoutbuflen, char *stderrbuf, int stderrbuflen) {
+	FILE *stdoutf, *stderrf;
+	int stdoutp[2], stderrp[2];
+	int r;
+
+	if(pipe(stdoutp) == -1)
+		die("pipe: %s\n", strerror(errno));
+
+	if(pipe(stderrp) == -1)
+		die("pipe: %s\n", strerror(errno));
+
+	pid = fork();
+	if(pid == -1)
+		die("fork: %s\n", strerror(errno));
+	else if(pid == 0) {
+		close(STDOUT_FILENO);
+		dup(stdoutp[1]);
+		close(STDERR_FILENO);
+		dup(stderrp[1]);
+
+		execl("/bin/sh", "/bin/sh", "-c", cmd, NULL);
+		die("/bin/sh: %s", strerror(errno));
+	}
+
+	struct pollfd ufds[2] = {
+		{ stdoutp[0], POLLIN, 0 },
+		{ stderrp[0], POLLIN, 0 },
+	};
+
+	stdoutf = fdopen(stdoutp[0], "r");
+	stderrf = fdopen(stderrp[0], "r");
+	stdoutbuf[0] = '\0';
+	stderrbuf[0] = '\0';
+
+	for(;;) {
+		if((r = poll(ufds, 2, 2000)) == -1) {
+			if(errno != EINTR)
+				die("poll: %s\n", strerror(errno));
+			if(pid == -1)
+				break;
+		}
+		
+		if(r == 0) /* timed out */
+			break;
+
+		/* TODO: check for error in .revents */
+		if(ufds[0].revents & POLLIN) {
+			if(!strlen(stdoutbuf)) {
+				fgets(stdoutbuf, stdoutbuflen, stdoutf);
+				/* only need one line; break
+				 * NOTE: if command is broken the process might
+				 * continue forever. */
+				break;
+			}
+		}
+		if(ufds[1].revents & POLLIN) {
+			fgets(stderrbuf, stderrbuflen, stderrf);
+		}
+
+		/* if process died we are finished (see sigchld())*/
+		if(pid == -1)
+			break;
+	}
+
+	/* Clean up */
+	fclose(stdoutf);
+	fclose(stderrf);
+	close(stdoutp[0]);
+	close(stdoutp[1]);
+	close(stderrp[0]);
+	close(stderrp[1]);
+
+}
+
 void
 run() {
 	XEvent ev;
 	KeySym keysym;
 	char buf[256];
-	int i, len;
-	FILE *f;
+	char errbuf[256] = {"ERROR: "};
+	int i;
+	char *errmsg = NULL;
 
 	XSync(dpy, False);
 	while(!XNextEvent(dpy, &ev)) {
-		//puts("got event");
 		if(ev.type == KeyPress) {
-			//puts("\tkeypress!");
 			keysym = XKeycodeToKeysym(dpy, (KeyCode)ev.xkey.keycode, 0);
 			for(i=0; i < LENGTH(conf); i++) {
 				if(conf[i].key == keysym &&
 						CLEANMASK(conf[i].mod) == CLEANMASK(ev.xkey.state)) {
-					//printf("%i\n", (int)keysym);
-					//puts(conf[i].cmd);
-					f = popen(conf[i].cmd, "r");
-					if(!f) {
-						perror(conf[i].cmd);
-						break;
-					}
 
-					len = fread(buf, 1, sizeof(buf)-1, f);
-					buf[len - (buf[len-1] == '\n' ? 1 : 0)] = '\0';
-					fclose(f);
+					readcmd(conf[i].cmd, buf, sizeof buf, errbuf+7/*skip the 'ERROR: ' part of the buffer */, sizeof(errbuf)-7);
+
+					if(buf[strlen(buf)-1] == '\n')
+						buf[strlen(buf)-1] = '\0';
+					if(errbuf[strlen(errbuf)-1] == '\n')
+						errbuf[strlen(errbuf)-1] = '\0';
+
+					if(strlen(buf))
+						errmsg = NULL;
+					else if(strlen(errbuf+7))
+						errmsg = errbuf;
+					else
+						errmsg = "ERROR: Command failed";
 
 					if(!is_mapped) {
 						XMapRaised(dpy, win);
+						/* Wait for window to be mapped */
 						while (1) {
 							XMaskEvent(dpy, SubstructureNotifyMask, &ev);
 							if ((ev.type == CreateNotify || ev.type == MapNotify)
@@ -303,7 +391,7 @@ run() {
 						is_mapped = True;
 					}
 					start_timer();
-					conf[i].disp(&conf[i], &buf[0]);
+					conf[i].disp(&conf[i], errmsg ? errmsg : buf, errmsg ? True : False);
 				}
 			}
 		}
